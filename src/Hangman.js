ae63aa77da2bbd58b0f23b8f6ee47831ca8d5596
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-16 12:24:55 +0100

    added logic to ending the game

--- src/Hangman.js
+++ src/Hangman.js
@@ -58,6 +58,7 @@ class Hangman extends Component {
 
   /** render: render game */
   render() {
+    let gameOver = this.state.nWrong >= this.props.maxWrong;
     return (
       <div className="Hangman">
         <h1>Hangman</h1>
@@ -65,8 +66,14 @@ class Hangman extends Component {
         <p className="Hangman-guesses">
           Wrong guesses: {this.state.nWrong}/{this.props.maxWrong}
         </p>
-        <p className="Hangman-word">{this.guessedWord()}</p>
-        <p className="Hangman-btns">{this.generateButtons()}</p>
+        <p className="Hangman-word">
+          {!gameOver ? this.guessedWord() : this.state.answer}
+        </p>
+        <p className="Hangman-btns">
+          {!gameOver
+            ? this.generateButtons()
+            : `You lose! The word was ${this.state.answer}`}
+        </p>
       </div>
     );
   }
